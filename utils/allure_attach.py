0ea1074d47591debcdbfdf61b3f805bac88ae5f7
Vitaliy Zakharov <zak125@mail.ru>
2024-01-14 17:31:36 +0300

    if response is empty do nothing

--- utils/allure_attach.py
+++ utils/allure_attach.py
@@ -15,20 +15,21 @@ def response_logging(response: Response):
 
 
 def response_attaching(response: Response):
-    allure.attach(
-        body=response.request.url,
-        name="Request url",
-        attachment_type=AttachmentType.TEXT,
-    )
-    allure.attach(
-        body=json.dumps(response.request.body, indent=4, ensure_ascii=True),
-        name="Request body",
-        attachment_type=AttachmentType.JSON,
-        extension="json",
-    )
-    allure.attach(
-        body=json.dumps(response.json(), indent=4, ensure_ascii=True),
-        name="Response",
-        attachment_type=AttachmentType.JSON,
-        extension="json",
-    )
+    if response:
+        allure.attach(
+            body=response.request.url,
+            name="Request url",
+            attachment_type=AttachmentType.TEXT,
+        )
+        allure.attach(
+            body=json.dumps(response.request.body, indent=4, ensure_ascii=True),
+            name="Request body",
+            attachment_type=AttachmentType.JSON,
+            extension="json",
+        )
+        allure.attach(
+            body=json.dumps(response.json(), indent=4, ensure_ascii=True),
+            name="Response",
+            attachment_type=AttachmentType.JSON,
+            extension="json",
+        )
